73c98e0400e4dddda9f27b04ea5d18f415e919fc
Laurent LE CORRE <laurent@thecoredev.fr>
2020-03-08 11:57:59 +0100

    Renommage de fonctions principalement

--- js/events.js
+++ js/events.js
@@ -3,25 +3,22 @@ import {
     camera
 } from "./environment.js"
 
-
-const pickPosition = {
-    x: 0,
-    y: 0
-};
-
-function getCanvasRelativePosition(event) {
-    const rect = canvas.getBoundingClientRect();
-    return {
-        x: event.clientX - rect.left,
-        y: event.clientY - rect.top,
-    };
+export function onMouseClick(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    console.log("Clic sur X = " + mouse.x + " / Y = " + mouse.y);
 }
 
 
-export function handleClick(event) {
-    const pos = getCanvasRelativePosition(event);
-    pickPosition.x = (pos.x / canvas.clientWidth) * 2 - 1;
-    pickPosition.y = (pos.y / canvas.clientHeight) * -2 + 1; // note we flip Y
+export function onMouseMove(event) {
+    event.preventDefault();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
 
-    console.log("X = " + pickPosition.x + " / Y = " + pickPosition.y)
+
+export function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
